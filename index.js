1bc1ebb1d61008a3eb8cd705abdfc18a275f7737
Zev <zevgeller@gmail.com>
2022-06-02 17:21:56 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 function addingEventListener() {
+    const input = document.getElementById('button');
+
+input.addEventListener('click', clickAlert);
+
 }
+
+function clickAlert() {
+    alert('I was clicked!');
+  }
